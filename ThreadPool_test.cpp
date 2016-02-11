5309aa2f8d6718ddb8fcad4659da8d2af56f66ac
muupu <once645@gmail.com>
2016-02-11 11:55:16 +0800

    add: ThreadPoolol_test.cpp

---
+++ ThreadPool_test.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<string>
+#include<thread>
+#include<unistd.h>
+
+#include "ThreadPool.h"
+
+using namespace std;
+
+class TaskWork : public Task
+{
+public:
+    //Task() {}
+    virtual int Run()
+    {
+        printf("%s\n",(char *)this->TaskData);
+        sleep(5);
+        return 0;
+    }
+};
+
+int main()
+{
+    TaskWork task;
+    char obj[] = "abc";
+    task.SetData( obj );
+    task.showdata();
+    ThreadPool<TaskWork> thread_pool(10);
+
+    for( int i = 0; i < 20; i++ )    //line 33
+        thread_pool.PushTask( &task );
+
+    while( 1 ) { 
+        if( thread_pool.GetTaskNum() == 0 && thread_pool.StopAll() == -1 )     { 
+            cout << "exit the main.\n";
+            exit(0);
+        }
+    }   
+    sleep(2);
+
+    return 0;
+}
