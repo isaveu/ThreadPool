978ba474887675076f6388b024ad41ab59e40892
muupu <once645@gmail.com>
2016-02-22 23:30:36 +0800

    mod test

--- ThreadPool_test.cpp
+++ ThreadPool_test.cpp
@@ -8,14 +8,13 @@
 
 using namespace std;
 
-class TaskWork : public Task
+class TaskWork : public ITask
 {
 public:
-    //Task() {}
-    virtual int Run()
+    int Run() override
     {
-        printf("%s\n",(char *)this->TaskData);
-        std::this_thread::sleep_for(std::chrono::seconds(5)); 
+        printf("thread id:%lu\n", std::this_thread::get_id());
+        //std::this_thread::sleep_for(std::chrono::seconds(5)); 
         return 0;
     }
 };
@@ -23,13 +22,10 @@ public:
 void test_ThreadPool()
 {
     TaskWork task;
-    char obj[] = "abc";
-    task.SetData(obj);
-    //task.showdata();
-    ThreadPool<TaskWork> thread_pool(10);
+    ThreadPool thread_pool(10);
 
-    for( int i = 0; i < 20; i++ )    //line 33
-        thread_pool.AddTask( &task );
+    for( int i = 0; i < 20; i++ )
+        thread_pool.AddTask(&task);
 
     while( 1 ) { 
         if( thread_pool.GetTaskNum() == 0 && thread_pool.StopAll() == -1 )     { 
